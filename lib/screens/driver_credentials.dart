a3b36a25d40eb2966bee6dfec15df458a3baf837
Samiul Hossen Sarkar Santo <samiulhossen2254901079@student.bup.edu.bd>
2025-05-08 11:38:26 +0600

    updated driver registration setup

--- lib/screens/driver_credentials.dart
+++ lib/screens/driver_credentials.dart
@@ -1,6 +1,3 @@
 const Map<String, String> driverCredentials = {
-  "asdgt1": "BupBus@2008",
-  "asdgt2": "BupBus@2008",
-  "ctclg1": "BupBus@2008",
-  "ctclg2": "BupBus@2008",
+  "driver": "bupbus2025",
 };
